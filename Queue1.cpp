887cc83ac0a5a07bf11190d38fb7bed1e34140d8
Elena <gotoheaven@yandex.ru>
2017-05-23 11:07:36 +0300

    first

---
+++ Queue1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include "Queue1.h"
+
+int main()
+{
+	TQueue <int> A;
+	TStack <int> B;
+	A.Push (1);
+	A.Push (2);
+	A.Push (3);
+	A.Push (4);
+	A.Pop ();
+
+	B.Push (1);
+	B.Pop ();
+	B.Push (2);
+	B.Push (3);
+	B.Push (4);
+	B.Pop ();
+
+
+
+return 1;
+
+}
+
+
+      
